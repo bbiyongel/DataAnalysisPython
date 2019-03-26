40ea8759b532d0eb66b94df08b879e0eb2265cca
kangraemin <ee4787@naver.com>
2019-03-26 18:12:03 +0900

    2019-03-14

--- BaseLine.py
+++ BaseLine.py
@@ -33,16 +33,91 @@ test = test.drop("id", axis=1)
 print("train.sahpe = ", train.shape)
 print("test.shape = ", test.shape)
 
+# oulier 탐색 / 제거
+
+# sqft_lot15 outlier 탐색
+sqft_lot15_threshold = 800000
+train_temp_copy_del_sqft_lot15 = train.__deepcopy__()
+train_temp_copy_del_sqft_lot15['sqft_lot15'] = train_temp_copy_del_sqft_lot15['sqft_lot15'].map(lambda x: np.nan if x < sqft_lot15_threshold else x)
+train_temp_copy_del_sqft_lot15 = train_temp_copy_del_sqft_lot15.dropna(axis=0)
+print(train_temp_copy_del_sqft_lot15)
+
+# sqft_lot15의 outlier 제거
+train['sqft_lot15'] = train['sqft_lot15'].map(lambda x: np.nan if x > sqft_lot15_threshold else x)
+train = train.dropna(axis=0)
+
+# sqft_lot outlier 탐색
+sqft_lot_threshold = 1500000
+train_temp_copy_del_sqft_lot = train.__deepcopy__()
+train_temp_copy_del_sqft_lot['sqft_lot'] = train_temp_copy_del_sqft_lot['sqft_lot'].apply(lambda x: np.nan if x < sqft_lot_threshold else x)
+train_temp_copy_del_sqft_lot = train_temp_copy_del_sqft_lot.dropna(axis=0)
+print(train_temp_copy_del_sqft_lot)
+
+# sqft_lot의 outlier 제거
+train['sqft_lot'] = train['sqft_lot'].map(lambda x: np.nan if x > sqft_lot_threshold else x)
+train = train.dropna(axis=0)
+# Shape of Data
+print("train.sahpe = ", train.shape)
+print("test.shape = ", test.shape)
+
+# sqft_living outlier 탐색
+sqft_living_threshold = 12000
+train_temp_copy_del_sqft_living = train.__deepcopy__()
+train_temp_copy_del_sqft_living['sqft_living'] = train_temp_copy_del_sqft_living['sqft_living'].apply(lambda x: np.nan if x < sqft_living_threshold else x)
+train_temp_copy_del_sqft_living = train_temp_copy_del_sqft_living.dropna(axis=0)
+print(train_temp_copy_del_sqft_living)
+
+# sqft_lot의 outlier 제거
+train = train.drop(8912)
+print(train.isnull().sum())
+
+# sqft_above outlier 탐색
+# 앞에서 이미 삭제된듯
+sqft_above_threshold = 8000
+train_temp_copy_del_sqft_above = train.__deepcopy__()
+train_temp_copy_del_sqft_above['sqft_above'] = train_temp_copy_del_sqft_above['sqft_above'].apply(lambda x: np.nan if x < sqft_above_threshold else x)
+train_temp_copy_del_sqft_above = train_temp_copy_del_sqft_above.dropna(axis=0)
+print(train_temp_copy_del_sqft_above)
+
+# grade outlier 탐색
+grade_price_threshold = 6500000
+train_temp_copy_del_grade_price = train.__deepcopy__()
+train_temp_copy_del_grade_price['grade'] = train_temp_copy_del_grade_price['grade'].apply(lambda x: np.nan if x != 11 else x )
+train_temp_copy_del_grade_price['price'] = train_temp_copy_del_grade_price['price'].apply(lambda x: np.nan if x < grade_price_threshold else x)
+train_temp_copy_del_grade_price = train_temp_copy_del_grade_price.dropna(axis=0)
+print(train_temp_copy_del_grade_price)
+train.drop(2775)
+
+# bathroom outlier 탐색
+# 2775 앞에서 지워짐
+bathroom_price_threshold = 6500000
+train_temp_copy_del_bathroom_price = train.__deepcopy__()
+train_temp_copy_del_bathroom_price['bathrooms'] = train_temp_copy_del_bathroom_price['bathrooms'].apply(lambda x: np.nan if x != 4.5 else x )
+train_temp_copy_del_bathroom_price['price'] = train_temp_copy_del_bathroom_price['price'].apply(lambda x: np.nan if x < bathroom_price_threshold else x)
+train_temp_copy_del_bathroom_price = train_temp_copy_del_bathroom_price.dropna(axis=0)
+print(train_temp_copy_del_bathroom_price)
+
+# Shape of Data
+print("train.sahpe = ", train.shape)
+print("test.shape = ", test.shape)
+
 from sklearn.model_selection import StratifiedShuffleSplit
 
 # 나누려고하는 범주의 갯수를 확인하자 ( 갯수가 2보다 작으면 ( 1이면 ) 나누어 질 수 없다 -> 상식적인것 )
+# n_split -> iteration 횟수
+
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(train, train['condition']):
     train_split = train.loc[train_index]
     train_test_split = train.loc[test_index]
 
+from sklearn.model_selection import train_test_split as train_test_lib
+
+# Random split
+train_split, train_test_split = train_test_lib(train, test_size=0.2, random_state=42)
+
 # Shape of Data
-print("train_split.sahpe = ", train.shape)
+print("train_split.sahpe = ", train_split.shape)
 print("train_test_split.shape = ", train_test_split.shape)
 
 # Data's Head
@@ -51,8 +126,14 @@ print("train_test_split.shape = ", train_test_split.shape)
 
 # Date Data Check ( T000000가 없는것은 없는지 )
 cnt = 0
+temp = 0
+print("asdf")
+print(train.isnull().sum())
+print(train_split.isnull().sum())
+print(train_test_split.isnull().sum())
 for i in train_split["date"]:
     if i[8:] == "T000000":
+        temp += 1
         continue
     else:
         cnt = cnt + 1
@@ -65,6 +146,8 @@ else:
 # date 변경
 train_split['date'] = train_split['date'].apply(lambda x: x[0:8]).astype(int)
 train_test_split['date'] = train_test_split['date'].apply(lambda x: x[0:8]).astype(int)
+test['date'] = test['date'].apply(lambda x: x[0:8]).astype(int)
+train['date'] = train['date'].apply(lambda x: x[0:8]).astype(int)
 
 # 결측치 탐색
 # print(train_split.isnull().sum())
@@ -75,9 +158,12 @@ f, ax = plt.subplots(figsize=(8, 6))
 sns.distplot(train_split['price'])
 # plt.show()
 
-train_split['price'] = np.log1p(train_split['price'])
+# log 변환
+train_split['price'] = np.log(train_split['price'])
+train['price'] = np.log(train['price'])
 f, ax = plt.subplots(figsize=(8, 6))
 sns.distplot(train_split['price'])
+
 
 
 # plt.show()
@@ -121,6 +207,7 @@ for i in attributes_continuous:
     show_target_scatter_plot(i)
     # plt.show()
 
+# 폐기 코드
 # for i in range(0,len(train_split['yr_renovated'])):
 #     if train_split['yr_renovated'][i] == 0:
 #         train_split['yr_renovated'][i] = train_split['yr_built'][i]
@@ -138,16 +225,15 @@ lin_reg = LinearRegression()
 lin_reg.fit(train_split[train_columns], train_split['price'])
 
 pred = lin_reg.predict(train_test_split[train_columns])
-adjusted_pred = np.exp(pred)+1
+adjusted_pred = np.exp(pred)
 
 from sklearn.metrics import mean_squared_error
 
+print(train_test_split.isnull().any())
+print(np.where(np.isnan(train_test_split)))
 lin_mse = mean_squared_error(train_test_split['price'], adjusted_pred)
 lin_rmse = np.sqrt(lin_mse)
 print(lin_rmse)
-
-# np.savetxt('predict.csv',pred,delimiter=',')
-# print(pred)
 
 import lightgbm as lgb
 from sklearn.metrics import mean_squared_error
@@ -182,6 +268,8 @@ y_reg = train_split['price']
 folds = KFold(n_splits=5, shuffle=True, random_state=42)
 oof = np.zeros(len(train_split))
 predictions = np.zeros(len(train_test_split))
+predictions_submission = np.zeros(len(test))
+
 feature_importance_df = pd.DataFrame()
 
 # run model
@@ -195,7 +283,6 @@ for fold_, (trn_idx, val_idx) in enumerate(folds.split(train_split)):
     clf = lgb.train(param, trn_data, num_round, valid_sets=[trn_data, val_data], verbose_eval=500,
                     early_stopping_rounds=100)
     oof[val_idx] = clf.predict(train_split.iloc[val_idx][train_columns], num_iteration=clf.best_iteration)
-    # oof[val_idx] = np.exp(clf.predict(train_split.iloc[val_idx][train_columns], num_iteration=clf.best_iteration))+1
     # feature importance
     fold_importance_df = pd.DataFrame()
     fold_importance_df["Feature"] = train_columns
@@ -203,8 +290,51 @@ for fold_, (trn_idx, val_idx) in enumerate(folds.split(train_split)):
     fold_importance_df["fold"] = fold_ + 1
     feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
     # predictions
-    # predictions += np.exp(clf.predict(train_test_split[train_columns], num_iteration=clf.best_iteration) / folds.n_splits)+1
     predictions += clf.predict(train_test_split[train_columns], num_iteration=clf.best_iteration) / folds.n_splits
+    predictions_submission += clf.predict(test[train_columns], num_iteration=clf.best_iteration) / folds.n_splits
 
-cv = np.sqrt(mean_squared_error(np.exp(oof)+1, np.exp(y_reg))+1)
+# oof -> training set의 예측결과 y_reg -> training set의 가격변수
+cv = np.sqrt(mean_squared_error(np.exp(oof), np.exp(y_reg)))
 print(cv)
+
+# predictions -> test_set 예측결과
+cv_test = np.sqrt(mean_squared_error(train_test_split['price'], np.exp(predictions)))
+print(cv_test)
+
+np.savetxt('predict.csv', np.exp(predictions_submission), delimiter=',')
+# print(pred)
+
+# 제출용 코드
+y_reg = train['price']
+
+# prepare fit model with cross-validation
+folds = KFold(n_splits=5, shuffle=True, random_state=42)
+oof = np.zeros(len(train))
+predictions = np.zeros(len(test))
+feature_importance_df = pd.DataFrame()
+
+# run model
+for fold_, (trn_idx, val_idx) in enumerate(folds.split(train)):
+    trn_data = lgb.Dataset(train.iloc[trn_idx][train_columns],
+                           label=y_reg.iloc[trn_idx])  # , categorical_feature=categorical_feats)
+    val_data = lgb.Dataset(train.iloc[val_idx][train_columns],
+                           label=y_reg.iloc[val_idx])  # , categorical_feature=categorical_feats)
+
+    num_round = 10000
+    clf = lgb.train(param, trn_data, num_round, valid_sets=[trn_data, val_data], verbose_eval=500,
+                    early_stopping_rounds=100)
+    oof[val_idx] = clf.predict(train.iloc[val_idx][train_columns], num_iteration=clf.best_iteration)
+    # feature importance
+    fold_importance_df = pd.DataFrame()
+    fold_importance_df["Feature"] = train_columns
+    fold_importance_df["importance"] = clf.feature_importance()
+    fold_importance_df["fold"] = fold_ + 1
+    feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+    # predictions
+    predictions += clf.predict(test[train_columns], num_iteration=clf.best_iteration) / folds.n_splits
+
+cv = np.sqrt(mean_squared_error(oof, y_reg))
+print(cv)
+
+np.savetxt('predict.csv',np.exp(predictions),delimiter=',')
+print(pred)
